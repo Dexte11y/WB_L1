655ee0a8193aa92b556c63b6259d82c2e819f4eb
dexte11y <d.federuakin@inbox.ru>
2024-05-05 23:11:37 +0300

    task2

--- task2/main.go
+++ task2/main.go
@@ -1,1 +1,21 @@
+// Написать программу, которая конкурентно рассчитает
+// значение квадратов чисел взятых из массива (2,4,6,8,10)
+// и выведет их квадраты в stdout.
+
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	arr := [5]int{2, 4, 6, 8, 10}
+	for _, v := range arr {
+		go func() {
+			squareNumbers := v * v
+			fmt.Println(squareNumbers)
+		}()
+	}
+	time.Sleep(2 * time.Second)
+}
